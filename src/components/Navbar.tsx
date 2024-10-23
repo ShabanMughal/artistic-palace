0804ba9d476fb27eccb2c90fd007ec03d958c1b5
ShabanMughal <thandermughal@gmail.com>
2024-10-23 12:58:48 +0300

    Updated code with new changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,44 @@
 "use client";
 import { FloatingNav } from "./ui/floating-navbar";
-
-
-
+import {
+  IconHome,
+  IconShoppingBag,
+  IconShoppingBagExclamation,
+  IconShoppingBagHeart,
+} from "@tabler/icons-react";
 
 export function Navbar() {
-    
-
   const navItems = [
     {
       name: "Home",
       link: "/",
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
       name: "Classic",
       link: "/classic-products",
+      icon: (
+        <IconShoppingBag className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
     },
     {
       name: "Modern",
       link: "/modern-products",
+      icon: (
+        <IconShoppingBagExclamation className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
     },
     {
       name: "Others",
       link: "/other-products",
+      icon: (
+        <IconShoppingBagHeart className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
     },
   ];
   return (
     <div className="relative  w-full">
       <FloatingNav navItems={navItems} />
-
     </div>
   );
 }
-
--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -18,49 +18,53 @@ export const FloatingNav = ({
   navItems: {
     name: string;
     link: string;
+    icon?: JSX.Element;
   }[];
   className?: string;
 }) => {
   const { scrollYProgress } = useScroll();
   const [visible, setVisible] = useState(true);
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
   const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
-    if (localStorage.getItem('theme') === 'dark' || window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      setTheme('dark');
-      document.documentElement.classList.add('dark');
+    if (
+      localStorage.getItem("theme") === "dark" ||
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
     } else {
-      setTheme('light');
-      document.documentElement.classList.remove('dark');
+      setTheme("light");
+      document.documentElement.classList.remove("dark");
     }
   }, []);
 
   const handleLinkClick = (link: string) => {
-    setIsLoading(true); 
+    setIsLoading(true);
 
     setTimeout(() => {
-      setIsLoading(false); 
-      router.push(link); 
-    }, 2000); 
+      setIsLoading(false);
+      router.push(link);
+    }, 2000);
   };
 
   const toggleTheme = () => {
-    if (theme === 'dark') {
-      setTheme('light');
-      document.documentElement.classList.remove('dark');
-      localStorage.setItem('theme', 'light');
+    if (theme === "dark") {
+      setTheme("light");
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     } else {
-      setTheme('dark');
-      document.documentElement.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     }
   };
   useMotionValueEvent(scrollYProgress, "change", (current) => {
     if (typeof current === "number") {
       const direction = current! - scrollYProgress.getPrevious()!;
- 
+
       if (scrollYProgress.get() < 0.05) {
         setVisible(false);
       } else {
@@ -75,44 +79,50 @@ export const FloatingNav = ({
 
   return (
     <>
-    {isLoading && <Loader />}
-    <AnimatePresence mode="wait">
-      <motion.div
-        initial={{
-          opacity: 1,
-          y: -100,
-        }}
-        animate={{
-          y: visible ? 0 : -100,
-          opacity: visible ? 1 : 0,
-        }}
-        transition={{
-          duration: 0.2,
-        }}
-        className={cn(
-          "flex w-[100%] md:w-[60%] fixed top-10 inset-x-0 mx-auto gap-3 md:gap-10 border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4 transition-colors duration-500",
-          className
-        )}
-        >
-        {navItems.map((navItem: { name: string; link: string; icon?: JSX.Element }, idx: number) => (
-          <div
-          key={`link=${idx}`}
-          onClick={() => handleLinkClick(navItem.link)}
+      {isLoading && <Loader />}
+      <AnimatePresence mode="wait">
+        <motion.div
+          initial={{
+            opacity: 1,
+            y: -100,
+          }}
+          animate={{
+            y: visible ? 0 : -100,
+            opacity: visible ? 1 : 0,
+          }}
+          transition={{
+            duration: 0.2,
+          }}
           className={cn(
-            "relative dark:text-neutral-50 cursor-pointer items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+            "flex w-[100%] md:w-[60%] fixed top-10 inset-x-0 mx-auto gap-10 border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4 transition-colors duration-500",
+            className
           )}
-          >
-            <span className="md:text-sm text-xs">{navItem.name}</span>
-          </div>
-        ))}
-        <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
-          <span onClick={toggleTheme}>
-            {theme === 'dark' ? <FiSun /> : <FiMoon />}
-          </span>
-          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </button>
-      </motion.div>
-    </AnimatePresence>
-        </>
+        >
+          {navItems.map(
+            (
+              navItem: { name: string; link: string; icon?: JSX.Element },
+              idx: number
+            ) => (
+              <div
+                key={`link=${idx}`}
+                onClick={() => handleLinkClick(navItem.link)}
+                className={cn(
+                  "relative dark:text-neutral-50 cursor-pointer items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+                )}
+              >
+                <span className="block sm:hidden">{navItem.icon}</span>
+                <span className="hidden sm:block text-sm">{navItem.name}</span>
+              </div>
+            )
+          )}
+          <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+            <span onClick={toggleTheme}>
+              {theme === "dark" ? <FiSun /> : <FiMoon />}
+            </span>
+            <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+          </button>
+        </motion.div>
+      </AnimatePresence>
+    </>
   );
 };
